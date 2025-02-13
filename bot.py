c0743f18c0399e80eb93d0a6ef33e78325edcbe7
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-02-14 00:23:05 +0100

    feat: added custom help bc why the hell not

--- bot.py
+++ bot.py
@@ -15,6 +15,34 @@ graph_help = f"seperate shit with pipe |\n" \
              f"{prefix}graph sin(x)|1,2\n" \
              f"{prefix}graph sin(x)|cos(x)|x**2|3,5" \
 
+class CustomHelpCommand(commands.DefaultHelpCommand):
+    def __init__(self):
+        super().__init__()
+        self.no_category = "Commands"
+
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(
+            title="Send Help",
+            description="graphy bot graphs graphs thank you\nsource code at https://github.com/KreconyMakaron/graphy",
+            color=discord.Color.green()
+        )
+
+        for cog, commands_list in mapping.items():
+            command_names = [command.name for command in commands_list]
+            if command_names:
+                cog_name = cog.qualified_name if cog else self.no_category
+                embed.add_field(
+                    name=cog_name,
+                    value=", ".join(f"`{prefix}{name}`" for name in command_names),
+                    inline=False
+                )
+
+        embed.set_footer(text=f"Use {prefix}help <command> for more details.")
+
+        await self.get_destination().send(embed=embed)
+
+bot.help_command = CustomHelpCommand()
+
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
