c7fe02468a339158d44d1ae498211148695501c8
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-02-14 21:25:26 +0100

    feat: updated flake.nix to make nix run work

--- flake.nix
+++ flake.nix
@@ -4,22 +4,29 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
-    (flake-utils.lib.eachDefaultSystem
+    flake-utils.lib.eachDefaultSystem
       (system:
         let 
 					pkgs = import nixpkgs { inherit system; };
+          python = pkgs.python3.withPackages (ps: [
+            ps.numpy
+            ps.sympy
+            ps.matplotlib
+            ps.discordpy
+          ]);
         in
         {
 					devShells.default = pkgs.mkShell {
 						buildInputs = [
-              (pkgs.python3.withPackages (ps: [
-                ps.numpy
-                ps.sympy
-                ps.matplotlib
-                ps.discordpy
-              ]))
+              python
             ];
 					};
-				})
-    );
+
+          packages = {
+            myapp = pkgs.writeShellScriptBin "run" ''
+              ${python}/bin/python3 bot.py
+            '';
+            default = self.packages.${system}.myapp;
+          };
+				});
 }
